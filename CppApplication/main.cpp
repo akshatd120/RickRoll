adc67efecebc36aa05616bd45dc7350b5b0d836f
akshat-windows <akshat-windows@DESKTOP-L0EOFL1>
2022-03-01 22:49:05 +0530

    Add project files.

---
+++ CppApplication/main.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+using namespace std;
+char str;
+
+int main()
+{
+	cout << "Hello World..\nPress F to Exit: ";
+	u_input:cin >> str;
+	if (str == 'f' || str == 'F')
+	{
+		ShellExecuteA(NULL, "open", "https://www.youtube.com/watch?v=dQw4w9WgXcQ", NULL, NULL, SW_SHOWDEFAULT);
+		return 0;
+	}
+	else
+	{
+		goto u_input;
+	}
+}
---
+++ CppApplication/pch.h
@@ -0,0 +1,6 @@
+﻿#pragma once
+#pragma comment (lib, "Shell32")
+#include <Windows.Foundation.h>
+#include <Windows.Foundation.Collections.h>
+#include <iostream>
+#include <shellapi.h>
