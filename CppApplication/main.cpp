e7de40261e5719166d74e1c64936f75a5a1c9778
Akshat <98334833+akshatd120@users.noreply.github.com>
2023-03-08 00:20:33 +0530

    Update main.cpp

--- CppApplication/main.cpp
+++ CppApplication/main.cpp
@@ -1,11 +1,11 @@
 ﻿#include "pch.h"
 using namespace std;
-char str;
 
 int main()
 {
-	cout << "Hello World..\nPress F to Exit: ";
-	u_input:cin >> str;
+	char str;
+	cout << "Press 'F' to see something fun ;)" << std::endl;
+	u_input : cin >> str;
 	if (str == 'f' || str == 'F')
 	{
 		ShellExecuteA(NULL, "open", "https://www.youtube.com/watch?v=dQw4w9WgXcQ", NULL, NULL, SW_SHOWDEFAULT);
